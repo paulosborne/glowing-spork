ed5fdf9a443e2bfed2fd4d8123ce110afa0c06c4
Paul Osborne <paul.osborne.uk@gmail.com>
2020-11-21 06:31:04 +0000

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,23 +3,18 @@ import { observer } from 'mobx-react';
 import { useRootStore } from '../../hooks';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
-
 import DatePicker from 'react-datepicker';
 import ClubPicker from '../ClubPicker/ClubPicker';
 
 const Header = observer(() => {
   const { clubs, date, setDate } = useRootStore();
 
-  const goHome = () => {
-    clubs.selectClub(null);
-  };
-
   return (
     <header className="flex flex-col sm:flex-row bg-gray-800 p-3 shadow-sm w-full">
       <div>
         <div className="flex pr-3">
           <div className="mr-3">
-            <button onClick={goHome} className="">
+            <button onClick={() => clubs.clearSelected()}>
               <FontAwesomeIcon icon={faHome} color="white" />
             </button>
           </div>
